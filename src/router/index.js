d47ade1bd51871c4baa84aef296405426cd95d72
minchoi999 <minkyuchoi999@gmail.com>
2020-06-02 15:13:21 -0700

    made request to clanController to get clan info. must be refactored later to include service files

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,43 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+// import Clan from '../views/Clan.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
+    redirect: '/home'
+  },
+  {
+    path: '/clan',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/clan/:id',
+    name: 'clan',
+    component: () => import(/* webpackChunkName: "clan" */ '../views/Clan.vue')
   }
 ]
 
+// If i want to split code w webPack, 
+// {
+//   path: '/clan/:id',
+//   name: 'clan',
+//   // route level code-splitting
+//   // this generates a separate chunk (about.[hash].js) for this route
+//   // which is lazy-loaded when the route is visited.
+//   component: () => import(/* webpackChunkName: "clan" */ '../views/Clan.vue')
+// }
+
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="clanTag"/>
+    <input type="submit" v-on:click="search"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      clanTag: "",
+      info: null
+    };
+  },
+  methods: {
+    search() {
+      // const clanTag = this.clanTag;
+      const clanTag = `http://localhost:3000/clan/${this.clanTag}`;
+      axios.get(clanTag).then(response => {
+        this.info = response;
+      });
+    }
   }
 }
 </script>
